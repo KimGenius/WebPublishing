847169e19777501d43e5189b8bd273ad949900ea
GeniusK <geniusk1047@naver.com>
2018-09-13 20:16:30 +0900

    slide

--- InteriorDesign/script.js
+++ InteriorDesign/script.js
@@ -3,7 +3,7 @@ $(function () {
     var id = $(this).attr('href')
     var scrollTop = id === '#' ? 0 : $(id).offset().top
     $("html, body").animate({
-      scrollTop
+      scrollTop: scrollTop
     }, 500)
   })
 })
