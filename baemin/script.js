1c925817a82c028bfd768f4bba9a2241d9ae31d3
GeniusK <geniusk1047@naver.com>
2018-09-18 00:36:20 +0900

    fullpage lib 추가 및 적용

--- baemin/script.js
+++ baemin/script.js
@@ -1,9 +1,15 @@
 $(function () {
-  $(window).scroll(function () {
-    if ($(this).scrollTop() === 0) {
-      $('.appDown-image').css('opacity', 0)
-    } else {
-      $('.appDown-image').css('opacity', 1)
+  $('#fullpage').fullpage({
+    //options here
+    autoScrolling: true,
+    anchors: ['content1', 'content2', 'content3'],
+    scrollHorizontally: true,
+    licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+    navigation: true,
+    navigationPosition: 'right',
+    onLeave: function (origin, destination, direction) {
+      if (destination.anchor === 'content1') $('.appDown-image').css('opacity', 0)
+      else $('.appDown-image').css('opacity', 1)
     }
-  })
+  });
 })
